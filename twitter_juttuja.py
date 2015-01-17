273ba94294d7cf86f9733bc64d7e33156225d39f
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-01-17 11:57:32 +0200

    Siisti koodia

--- twitter_juttuja.py
+++ twitter_juttuja.py
@@ -7,15 +7,15 @@ laskuri = {}
 
 for tiedosto in sys.argv[1:]:
 
-
-
     with open(tiedosto) as json_data:
         d = json.load(json_data)
         json_data.close()
+
         for tweet in d:
             aika = datetime.datetime.fromtimestamp(
                     int(tweet['time'])
                 ).strftime('%Y-%m-%d')
+
             if aika not in laskuri:
                 laskuri[aika] = 0
 
@@ -25,12 +25,5 @@ for tiedosto in sys.argv[1:]:
 
             ## print teema_lista
 
-    #pprint (laskuri)
-
-
-
-for x in sorted( laskuri.keys() ):
-
-    a = laskuri[x]
-
-    print x,",", a
+for paiva in sorted( laskuri.keys() ):
+    print paiva , "," , laskuri[ paiva ]
